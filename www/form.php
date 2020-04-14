5f2f398a11d929780e9e0b020a21a6d4a3941d8b
Zineddine BEOUCHE <beouche.zineddine@gmail.com>
2020-04-15 01:49:47 +0200

    Amélioration du formulaire de calcul de score

--- www/form.php
+++ www/form.php
@@ -45,6 +45,7 @@
         border-color: lightgray;
       }
 
+
     </style>
   </head>
   <body>
@@ -58,74 +59,80 @@
 
             <div class="form-group row">
 
-              <div class="col-12">
+              <div class="col-sm-6 text-center">
                 <label for="pt_victoire">Points de victoire</label>
                 <input type="number" name="pt_victoire" id="pt_victoire" min="0"/>
               </div>
 
-              <div class="col-sm-6">  
+              <div class="col-sm-6 text-center">
+              </div>
+
+              <div class="col-sm-6 text-center">  
                 <label for="c_armee">Cartes armées</label>
                 <input type="number" name="c_armee" id="c_armee" min="0"/>
               </div>
 
-              <div class="col-sm-6"> 
+              <div class="col-sm-6 text-center"> 
                 <label for="m_armee">X armée</label>
                 <input type="number" name="m_armee" id="m_armee" min="0"/>
               </div>
 
-              <div class="col-sm-6">  
+              <div class="col-sm-6 text-center">  
                 <label for="c_science">Cartes sciences</label>
                 <input type="number" name="c_science" id="c_science" min="0"/>
               </div>
 
-              <div class="col-sm-6"> 
+              <div class="col-sm-6 text-center"> 
                 <label for="m_science">X science</label>
                 <input type="number" name="m_science" id="m_science" min="0"/>
               </div>
 
-              <div class="col-sm-6">
+              <div class="col-sm-6 text-center">
                 <label for="c_economie">Cartes économies</label>
                 <input type="number" name="c_economie" id="c_economie" min="0"/>
               </div>
 
-              <div class="col-sm-6"> 
+              <div class="col-sm-6 text-center"> 
                 <label for="m_economie">X economie</label>
                 <input type="number" name="m_economie" id="m_economie" min="0"/>
               </div>
 
-              <div class="col-sm-6">
+              <div class="col-sm-6 text-center">
                 <label for="c_merveille">Cartes merveilles</label>
                 <input type="number" name="c_merveille" id="c_merveille" min="0"/>
               </div>
 
-              <div class="col-sm-6"> 
+              <div class="col-sm-6 text-center"> 
                 <label for="m_merveille">X merveille</label>
                 <input type="number" name="m_merveille" id="m_merveille" min="0"/>
               </div>
 
-              <div class="col-sm-6">
+              <div class="col-sm-6 text-center">
                 <label for="j_trader">Jetons traders</label>
                 <input type="number" name="j_trader" id="j_trader" min="0"/>
               </div>
 
-              <div class="col-sm-6"> 
+              <div class="col-sm-6 text-center"> 
                 <label for="m_trader">X trader</label>
                 <input type="number" name="m_trader" id="m_trader" min="0"/>
               </div>
 
-              <div class="col-sm-6">
+              <div class="col-sm-6 text-center">
                 <label for="j_militaire">Jetons militaires</label>
                 <input type="number" name="j_militaire" id="j_militaire" min="0"/>
               </div>
 
-              <div class="col-sm-6"> 
+              <div class="col-sm-6 text-center"> 
                 <label for="m_militaire">X militaire</label>
                 <input type="number" name="m_militaire" id="m_militaire" min="0"/>
               </div>
-            </div>
+              <div class="col-sm-6 text-center">
+              </div>
 
-            <div class="col-sm-6">
-                <button type="submit" name="calculer" class="btn btn-primary">Calculer score</button>
+              <div class="col-sm-6 text-center">
+                <button type="submit" name="calculer" class="btn btn-primary mt-4">Calculer score</button>
+              </div>
+
             </div>
 
         </form>
