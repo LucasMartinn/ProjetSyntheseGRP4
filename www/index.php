6c55d7f8e3ad14997dac6ec385cd3ff511d89134
daveurandom <ludovic.kiefer7@etu.univ-lorraine.fr>
2020-04-20 19:12:18 +0200

    Ajout de la barre de login

--- www/index.php
+++ www/index.php
@@ -1,13 +1,17 @@
+<?php
+require_once("tpl/userbar.php");
+?>
 <!DOCTYPE html>
 <html lang="fr">
 <head>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, user-scalable=yes, initial-scale=1.0">
-	<title>Minotaure</title>
-	<link rel="icon" href="images/favicon.png" />
-	<!--<link rel="stylesheet" href="" media="screen"/>-->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, user-scalable=yes, initial-scale=1.0">
+    <title>Minotaure</title>
+    <link rel="icon" href="images/favicon.png" />
+    <!--<link rel="stylesheet" href="" media="screen"/>-->
 </head>
 <body>
+<?= $userbar ?>
 <h1>Minotaure</h1>
 
 <?php
